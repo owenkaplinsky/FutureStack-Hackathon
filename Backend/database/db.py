fb20262aa8927b1b11be0daebf50fb6e41ab2d39
bruh-9000 <bruh9000games@gmail.com>
2025-10-01 20:30:33 -0700

    Fix db issue

--- Backend/database/db.py
+++ Backend/database/db.py
@@ -4,7 +4,11 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(
+    DATABASE_URL,
+    echo=True,
+    pool_pre_ping=True
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
