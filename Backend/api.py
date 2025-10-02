9e013d0df17379138c13feee39436738ba51392f
bruh-9000 <bruh9000games@gmail.com>
2025-10-02 09:12:11 -0700

    API fix

--- Backend/api.py
+++ Backend/api.py
@@ -22,13 +22,9 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 # Allow frontend dev server and preflight (OPTIONS) including custom AUTH_KEY header
-origins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
