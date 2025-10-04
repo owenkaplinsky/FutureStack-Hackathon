bd2378c17a908a74846bd167cce9a4d38fcdcf02
anupreksha18 <intern.anu.negi>
2025-10-04 09:25:07 +0530

    Update App.js and add Docs page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -10,7 +10,7 @@ import Docs from './Components/Docs/Docs';
 function App() {
   return (
     <div>
-     <Dashboard />
+    
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
