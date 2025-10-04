5e8550e48c933817bf56dcb3d4d9945806937aba
anupreksha18 <intern.anu.negi>
2025-10-04 07:29:00 +0530

    Connect Dashboard to user_info API

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import ChatPage from './Components/ChatPage/Chatpage';
@@ -6,13 +5,25 @@ import LandingPage from './Components/LandingPage/LandingPage';
 import SignUpPage from './Components/SignUpPage/SignUpPage';
 import LoginPage from './Components/LoginPage/LoginPage';
 import { ProtectedRoute } from './Components/ProtectedRoute/ProtectedRoute';
+import Dashboard from './Components/Dashboard/Dashboard';
+
 function App() {
   return (
-    <div>
+    <div className="App">
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+
+       // Protected routes — requires login 
+         <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
         <Route
           path="/tasks"
           element={
@@ -27,9 +38,13 @@ function App() {
             <ProtectedRoute>
               <ChatPage />
             </ProtectedRoute>
-          }
-        />
+          } 
+         /> 
+
+        //Catch-all route for 404
+     <Route path="*" element={<h1 className="text-center mt-20 text-white">404 - Page Not Found</h1>} />
       </Routes>
+     
     </div>
   );
 }
